dfe8c639075291fe3297699d0a022eccf5a0d1e0
Mritunjay077 <www.mukulmishra@gmail.com>
2024-11-21 10:49:25 +0530

    Update route.js

--- Server/route.js
+++ Server/route.js
@@ -1,23 +1,24 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
 
-// Use CORS to allow requests from different origins (adjust settings as needed)
 app.use(cors());
-
-// Built-in Express middleware for parsing JSON and form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
-// Import routes for different API endpoints
+// files
 const login = require("./api/login.js");
 const googleform = require("./api/googleform.js");
 const userinfo = require('./api/userrInfo.js');
 const contact = require('./api/contact.js');
 
-// Define your API routes
+// routes
 app.use("/login", login);
-app.use("/googleform", googleform);  // Route for handling Google form data
+app.use("/googleform", googleform);  // Updated route to googleform
 app.use("/userinfo", userinfo);
 app.use("/contact", contact);
 
